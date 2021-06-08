a63157a57cc3207a2a58590dbf4da4978762b4dc
Loki-it <60159705+Loki-it@users.noreply.github.com>
2021-06-08 23:11:07 +0200

    Log!
    Nella cartella Log ogni giorno si genererà un nuovo file di log con all'interno tutta l'attività svolta quel giorno

--- casualità.py
+++ casualità.py
@@ -1,11 +1,16 @@
 import random
 from datetime import datetime
 
-adesso = datetime.now()
-ora = adesso.strftime("%H:%M:%S")
 s = open("scelte.txt","r").read().splitlines()
-
 input("- Premi invio per generare -\n")
+
 while True:
-    print(ora,">",random.choice(s))
+    adesso = datetime.now()
+    oggi = adesso.strftime("%Y-%m-%d")
+    ora = adesso.strftime("%H:%M:%S")
+    x = random.choice(s)
+    print(ora,">",x)
+    with open("log/" + oggi + ".txt","a") as log:
+        log.writelines("\n" + ora + " > " + x)
+        log.close()
     input("")
